168530f381f1bdffdf3ac2e94b5e25f967cdcab8
Deepika V <deepikavelmurugan58@gmail.com>
2026-08-04 08:49:59 +0530

    Create FindMissingElements.cpp

---
+++ Arrays/FindMissingElements.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findMissingElements(vector<int>& nums) {
+        int mn = *min_element(nums.begin(), nums.end());
+        int mx = *max_element(nums.begin(), nums.end());
+        unordered_set<int> s(nums.begin(), nums.end());
+        vector<int> missing;
+
+        for (int i = mn; i <= mx; ++i) {
+            if (s.find(i) == s.end()) {
+                missing.push_back(i);
+            }
+        }
+        return missing;
+    }
+};
