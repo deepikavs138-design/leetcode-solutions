70f4af1a08c26d94a4b563b8bf61e2b7fd9a79b5
Deepika V <deepikavelmurugan58@gmail.com>
2026-08-09 22:36:23 +0530

    Create StoneGameII.cpp

---
+++ StoneGameII.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int stoneGameII(vector<int>& piles) {
+        int n = piles.size();
+        vector<int> suffixSum(n);
+        suffixSum[n - 1] = piles[n - 1];
+        for (int i = n - 2; i >= 0; --i)
+            suffixSum[i] = piles[i] + suffixSum[i + 1];
+
+        vector<vector<int>> dp(n, vector<int>(n + 1, 0));
+
+        function<int(int, int)> dfs = [&](int i, int M) {
+            if (i == n) return 0;
+            if (2 * M >= n - i) return suffixSum[i];
+            if (dp[i][M] != 0) return dp[i][M];
+
+            int best = 0;
+            for (int x = 1; x <= 2 * M; ++x)
+                best = max(best, suffixSum[i] - dfs(i + x, max(M, x)));
+
+            return dp[i][M] = best;
+        };
+
+        return dfs(0, 1);
+    }
+};
